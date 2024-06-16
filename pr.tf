2e56970e566f79db6620af55f1f5ca011dbefc98
NISHAN-THAPA <nishanyo1234@gmail.com>
2024-06-16 14:57:44 +0000

    Added pr.tf

---
+++ pr.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Define the provider
+provider "aws" {
+  region = "sa-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Define the provider
+provider "aws" {
+  region = "sa-east-1"
+}
+}
